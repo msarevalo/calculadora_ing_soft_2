03f248357b1820cf41c8c22a5f80b3b122566b6b
msarevalo <msarevalo@unbosque.edu.co>
2017-08-03 16:38:31 -0500

    Creacion de menu de operaciones

--- logica.java
+++ logica.java
@@ -1,14 +1,72 @@
 package calculadora;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class logica {
 
+	static Scanner teclado = new Scanner(System.in);
+	static Scanner teclado2 = new Scanner(System.in);
+	
+	public static void seleccion() {
+		int menu =0;
+		double numero1,numero2;
+		String cadena;
+		
+		while(menu != 6) {
+			System.out.println("1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Operacion Compleja\n6. Salir");
+			menu = teclado.nextInt();
+			switch (menu) {
+			case 1:
+				System.out.println("Escriba el primer numero:");
+				numero1 = teclado.nextDouble();
+				System.out.println("Escriba el segundo numero:");
+				numero2 = teclado.nextDouble();
+				System.out.println("El resultado de la suma es: " + operaciones.suma(numero1, numero2) + "\n");
+				break;
+			case 2:
+				System.out.println("Escriba el primer numero:");
+				numero1 = teclado.nextDouble();
+				System.out.println("Escriba el segundo numero:");
+				numero2 = teclado.nextDouble();
+				System.out.println("El resultado de la resta es: " + operaciones.resta(numero1, numero2) + "\n");
+				break;
+			case 3:
+				System.out.println("Escriba el primer numero:");
+				numero1 = teclado.nextDouble();
+				System.out.println("Escriba el segundo numero:");
+				numero2 = teclado.nextDouble();
+				System.out.println("El resultado de la multiplicacion es: " + operaciones.multiplicacion(numero1, numero2) + "\n");
+				break;
+			case 4:
+				System.out.println("Escriba el primer numero:");
+				numero1 = teclado.nextDouble();
+				System.out.println("Escriba el segundo numero:");
+				numero2 = teclado.nextDouble();
+				System.out.println("El resultado de la division es: " + operaciones.division(numero1, numero2) + "\n");
+				break;
+			case 5:
+				System.out.println("\n\nEscriba la operacion que se desea hacer");
+				cadena = teclado2.nextLine();
+				//System.out.println(cadena);
+				logica.separar(cadena);
+				break;
+			case 6:
+				System.out.println("Gracias por usar la calculadora...");
+				break;
+			default:
+				System.out.println("Ingrese una opcion valida\n");
+				menu = 0;
+				break;
+			}
+		}
+	}
+	
 	public static void separar(String cadena) {
 		String[] operadores = null;
 		String[] numeros = null;
 		int contador = 1;
-		double numero = 0;
+		double numeroarreglo = 0;
 		Double resultado = 0.0;
 		char respuesta = 's';
 		ArrayList<Double> numerosfin = new ArrayList<Double>();
@@ -16,13 +74,13 @@ public class logica {
 		operadores = cadena.split("[0-9]");
 		numeros = cadena.split("[-*/+]");
 
-		// contador = numeros[0].length();
+		//contador = numeros[0].length();
 
 		for (int i = 0; i < numeros.length; i++) {
 			//numero = Integer.parseInt(numeros[i]);
-			numero = Double.parseDouble(numeros[i]);
-			// System.out.println(numero);
-			numerosfin.add(i, numero);
+			numeroarreglo = Double.parseDouble(numeros[i]);
+			//System.out.println(numeroarreglo);
+			numerosfin.add(i, numeroarreglo);
 			// System.out.println(numerosfin.get(i));
 		}
 
@@ -73,6 +131,6 @@ public class logica {
 
 		//}
 
-		System.out.printf("\nEl resultado de " + cadena + " es igual a: %.2f", resultado);
+		System.out.printf("El resultado de " + cadena + " es igual a: %.2f\n\n", resultado);
 	}
 }
--- principal.java
+++ principal.java
@@ -5,26 +5,82 @@ import java.util.Scanner;
 public class principal {
 
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		String ciclo = "cont";
-		System.out.print("Escriba EXIT para salir");
-		while (ciclo.equals("cont")) {
+		//Scanner teclado = new Scanner(System.in);
+		//Scanner teclado2 = new Scanner(System.in);
+		//int seleccion = 0;
+		//double numero1 = 0.0;
+		//double numero2 = 0.0;
 
-			System.out.print("\n\nEscriba la operacion que se desea hacer");
-			String cadena = teclado.nextLine();
-
-			if (cadena.equals("EXIT") || cadena.equals("exit")) {
-				ciclo = "no";
+		logica.seleccion();
+		//logica.separar("4651+546+561-4651");
+		//System.out.print("1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Operacion Compleja\n6.Salir");
+		//seleccion = teclado.nextInt();
+		/*
+		int menu = 0;
+		double numero1;
+		double numero2;
+		String cadena;		
+		
+		while(menu != 6) {
+			System.out.println("1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Operacion Compleja\n6.Salir");
+			menu = teclado.nextInt();
+			switch (menu) {
+			case 1:
+				System.out.println("Escriba el primer numero:");
+				numero1 = teclado.nextDouble();
+				System.out.println("Escriba el segundo numero:");
+				numero2 = teclado.nextDouble();
+				System.out.println("El resultado de la suma es: " + operaciones.suma(numero1, numero2) + "\n");
+				break;
+			case 2:
+				System.out.println("Escriba el primer numero:");
+				numero1 = teclado.nextDouble();
+				System.out.println("Escriba el segundo numero:");
+				numero2 = teclado.nextDouble();
+				System.out.println("El resultado de la resta es: " + operaciones.resta(numero1, numero2) + "\n");
+				break;
+			case 3:
+				System.out.println("Escriba el primer numero:");
+				numero1 = teclado.nextDouble();
+				System.out.println("Escriba el segundo numero:");
+				numero2 = teclado.nextDouble();
+				System.out.println("El resultado de la multiplicacion es: " + operaciones.multiplicacion(numero1, numero2) + "\n");
+				break;
+			case 4:
+				System.out.println("Escriba el primer numero:");
+				numero1 = teclado.nextDouble();
+				System.out.println("Escriba el segundo numero:");
+				numero2 = teclado.nextDouble();
+				System.out.println("El resultado de la division es: " + operaciones.division(numero1, numero2) + "\n");
+				break;
+			case 5:
+				System.out.println("\n\nEscriba la operacion que se desea hacer");
+				cadena = teclado2.nextLine();
+				System.out.println(cadena);
+				logica.separar(cadena);
+				break;
+			case 6:
 				System.out.println("Gracias por usar la calculadora...");
-			} else {
-				if (cadena.isEmpty()) {
+				break;
+			default:
+				System.out.println("Ingrese una opcion valida\n");
+				menu = 0;
+				break;
+			}
+		}*/
+		
+		/*System.out.print("\n\nEscriba la operacion que se desea hacer");
+		String cadena = teclado.nextLine();
 
-				} else {
-					logica.separar(cadena);
-				}
+		if (cadena.equals("EXIT") || cadena.equals("exit")) {
+			System.out.println("Gracias por usar la calculadora...");
+		} else {
+			if (cadena.isEmpty()) {
+			} else {
+				logica.separar(cadena);
 			}
 		}
-		teclado.close();
-	}
 
+		teclado.close();*/
+	}
 }
